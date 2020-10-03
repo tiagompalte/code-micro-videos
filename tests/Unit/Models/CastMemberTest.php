910f4b2d5e0ebb94be338aaf8372039fe9f6fe71
Tiago M Palte <tiagompalte@hotmail.com>
2020-10-03 13:39:52 -0300

    implementação do cast member

---
+++ tests/Unit/Models/CastMemberTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\CastMember;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tests\TestCase;
+
+class CastMemberTest extends TestCase
+{
+    private $castMember;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->castMember = new CastMember();
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            Uuid::class, SoftDeletes::class
+        ];
+        $castMemberTraits = array_keys(class_uses(CastMember::class));
+        $this->assertEquals($traits, $castMemberTraits);
+    }
+
+    public function testFillableAttribute()
+    {
+        $fillable = ['name', 'type'];
+        $this->assertEquals($fillable,$this->castMember->getFillable());
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['created_at', 'updated_at', 'deleted_at'];
+        $this->assertEqualsCanonicalizing($dates, $this->castMember->getDates());
+    }
+
+    public function testIncrementing()
+    {
+        $this->assertFalse($this->castMember->incrementing);
+    }
+}
